eb0e69ea9a40d302c3d3d9d4a9e08643dade1677
Pierre DGR <pierredgr@air-de-pierre.home>
2023-07-02 23:41:34 +0200

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tempfile
 import tiktoken
 from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex
 
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import LLMChain
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -47,7 +47,7 @@ if uploaded_file is not None:
         tmpfile.write(uploaded_file.getvalue())
         tmpfile_path = tmpfile.name
 
-    loader = UnstructuredPDFLoader(tmpfile_path)
+    loader = UnstructuredFileLoader(tmpfile_path)
     documents = loader.load()
     texts = text_splitter.split_documents(documents)
     docsearch = FAISS.from_documents(texts, embeddings)
